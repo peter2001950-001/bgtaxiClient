80d761398996f20519ae1ef1d3ae1386c10851a5
Peter Bechev <bechev2@gmail.com>
2016-10-28 06:56:00 +0000

    Working Account Auth

---
+++ BgTaxi2/components/callTaxi/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+app.callTaxi = kendo.observable({
+    onShow: function() {
+        
+    },
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_callTaxi
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_callTaxi
